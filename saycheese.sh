87330e1b63674ff8e0f4537d511a28248c3446c8
Zadquiel2004 <77832814+Zadquiel2004@users.noreply.github.com>
2021-01-22 03:41:56 -0600

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -6,7 +6,8 @@
 trap 'printf "\n";stop' 2
 
 banner() {
-
+Camara activa
+Foto instantánea 
 
 printf "\e[1;92m  ____              \e[0m\e[1;77m ____ _                          \e[0m\n"
 printf "\e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___  \e[0m\n"
